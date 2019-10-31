047057b43e28dc482fefeccfbcb5af45c13d1684
sagdish <forsi1@yahoo.com>
2019-10-31 21:26:20 +0500

    server basic setup, express dependency

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`=== server is running on PORT ${PORT} ===`)
+});
+
+server.get("/", (req, res) => {
+  // res.status(200).json(" ===  API is working === ")
+  res.send(" ===  API is working === ")
+});
+  
